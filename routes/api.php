18964edefd031a8cee4f092a167c4096d3752ed5
BriceTchams <michaeltchamou393@gmail.com>
2026-02-13 23:30:52 +0100

    ajout des routes du portefeuille

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthOtpController;
 use App\Http\Controllers\ChauffeurController;
+use App\Http\Controllers\PortefeuilleController;
+use App\Http\Controllers\TransactionPointsController;
+
+// use App\Models\Portefeuille;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +17,7 @@ use App\Http\Controllers\ChauffeurController;
 // --- AUTHENTIFICATION ---
 Route::post('/register', [AuthOtpController::class, 'register']);
 Route::post('/login', [AuthOtpController::class, 'login']);
-Route::post('/verify-otp', [AuthOtpController::class, 'verifyOtp']);
+Route::post('/verifyOtp', [AuthOtpController::class, 'verifyOtp']);
 
 //admin
 Route::group(['prefix' => 'admin'], function () {
@@ -21,12 +25,23 @@ Route::group(['prefix' => 'admin'], function () {
     Route::get('/chauffeurs/{id}', [ChauffeurController::class, 'show']);
     Route::patch('/chauffeurs/{id}/validate', [ChauffeurController::class, 'validateAccount']);
     Route::patch('/chauffeurs/{id}/status', [ChauffeurController::class, 'updateStatus']);
+
+    // porte feuille
+ Route::post('/wallet/show', [PortefeuilleController::class, 'show']);
+    Route::post('/wallet/recharge', [TransactionPointsController::class, 'recharge']);
+     Route::get('/wallet/historique', [TransactionPointsController::class, 'historique']);
+
 });
 
 //chauffeur
 Route::group(['prefix' => 'chauffeur'], function () {
-    Route::poatch('/location/{id}', [ChauffeurController::class, 'updateLocation']);
+    Route::patch('/location/{id}', [ChauffeurController::class, 'updateLocation']);
     Route::get('/profile/{id}', [ChauffeurController::class, 'show']);
+    //porteffeuille
+  Route::post('/wallet/show', [PortefeuilleController::class, 'show']);
+    Route::post('/wallet/recharge', [TransactionPointsController::class, 'recharge']);
+     Route::get('/wallet/historique', [TransactionPointsController::class, 'historique']);
+
 });
 
 //passager
@@ -34,4 +49,11 @@ Route::group(['prefix' => 'passager'], function () {
     Route::patch('/location/{id}', [ChauffeurController::class, 'updateLocation']);
     Route::get('/liste-chauffeurs', [ChauffeurController::class, 'index']);
     Route::get('/recherche/{id}', [ChauffeurController::class, 'show']);
+    //portefeuille
+    Route::post('/wallet/show', [PortefeuilleController::class, 'show']);
+    Route::post('/wallet/recharge', [TransactionPointsController::class, 'recharge']);
+     Route::get('/wallet/historique', [TransactionPointsController::class, 'historique']);
 });
+
+
+// 
