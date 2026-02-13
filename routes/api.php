8cf2d3fb5ea5f8bc8808c99cac938ebc1b585acb
BriceTchams <michaeltchamou393@gmail.com>
2026-02-13 22:23:26 +0100

    ajout des routes chauffeurs

--- routes/api.php
+++ routes/api.php
@@ -1,42 +1,37 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-// Importation du contrôleur pour l'utiliser dans les routes
 use App\Http\Controllers\AuthOtpController;
+use App\Http\Controllers\ChauffeurController;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+/*
+|--------------------------------------------------------------------------
+| API Routes (Sans Middlewares)
+|--------------------------------------------------------------------------
+*/
 
+// --- AUTHENTIFICATION ---
+Route::post('/register', [AuthOtpController::class, 'register']);
+Route::post('/login', [AuthOtpController::class, 'login']);
+Route::post('/verify-otp', [AuthOtpController::class, 'verifyOtp']);
 
-
-   Route::POST('/login' ,[AuthOtpController::class , 'login']
-    ) ;
-       Route::POST('/register' ,[AuthOtpController::class , 'register']
-    ) ;
-
-    Route::post('/verifyOtp' ,[AuthOtpController::class , 'verifyOtP']
-    );
-
-
-// Groupe de routes pour les passagers
-// Route::middleware(['role:passager'])->group(function () {
-    Route::get('/accueil', function(){
-        return response()->json(['message' => 'Profil passager']);
-    });
-    
-// });
-
-
-
-
-
-
-Route::middleware(['role:chauffeur'])->group(function () {
-    // Route::get('/mes-reservations', [TrajetController::class, 'mesReservations']);
+//admin
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('/chauffeurs', [ChauffeurController::class, 'index']);
+    Route::get('/chauffeurs/{id}', [ChauffeurController::class, 'show']);
+    Route::patch('/chauffeurs/{id}/validate', [ChauffeurController::class, 'validateAccount']);
+    Route::patch('/chauffeurs/{id}/status', [ChauffeurController::class, 'updateStatus']);
 });
 
-Route::middleware(['role:admin'])->group(function () {
-    // Route::get('/mes-reservations', [TrajetController::class, 'mesReservations']);
+//chauffeur
+Route::group(['prefix' => 'chauffeur'], function () {
+    Route::poatch('/location/{id}', [ChauffeurController::class, 'updateLocation']);
+    Route::get('/profile/{id}', [ChauffeurController::class, 'show']);
+});
+
+//passager
+Route::group(['prefix' => 'passager'], function () {
+    Route::patch('/location/{id}', [ChauffeurController::class, 'updateLocation']);
+    Route::get('/liste-chauffeurs', [ChauffeurController::class, 'index']);
+    Route::get('/recherche/{id}', [ChauffeurController::class, 'show']);
 });
